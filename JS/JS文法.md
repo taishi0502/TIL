d9c8049e2c5cf5f451b91b8fdf6a943c03831549
taishi0502 <kimutai_71@icloud.com>
2022-07-06 13:40:26 +0900

    データの型確認

--- JS/JS文法.md
+++ JS/JS文法.md
@@ -65,6 +65,20 @@ price --;
 console.log(price);
 //1200
 ```
+- Javascript データの型
+```
+console.log(typeof 'hello');
+console.log(typeof 5);
+console.log(typeof true);
+console.log(typeof undefined);
+console.log(typeof null);
+// til.js:3 string
+// til.js:4 number
+// til.js:5 boolean
+// til.js:6 undefined
+// til.js:7 object
+```
+
 
 
 
--- JS/til.js
+++ JS/til.js
@@ -1,16 +1,12 @@
 'use strict';
-
-let price = 500;
-// price = price += 100;
-price += 100;
-
-price *= 2;
-
-price += 1;
-price ++;
-
-price -= 1;
-price --;
-
-console.log(price);
-//1200
+// JavaScript データの型
+console.log(typeof 'hello');
+console.log(typeof 5);
+console.log(typeof true);
+console.log(typeof undefined);
+console.log(typeof null);
+// til.js:3 string
+// til.js:4 number
+// til.js:5 boolean
+// til.js:6 undefined
+// til.js:7 object
