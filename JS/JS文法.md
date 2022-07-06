69e8bcbb1255111de261473328a92c887d62c6f2
taishi0502 <kimutai_71@icloud.com>
2022-07-06 13:34:29 +0900

    変数を使った計算

--- JS/JS文法.md
+++ JS/JS文法.md
@@ -48,5 +48,23 @@ price = 170;
 console.log(price * 140);
 console.log(price * 160);
 ```
+- 変数を使った計算
+```
+let price = 500;
+// price = price += 100;
+price += 100;
+
+price *= 2;
+
+price += 1;
+price ++;
+
+price -= 1;
+price --;
+
+console.log(price);
+//1200
+```
+
 
 
--- JS/til.js
+++ JS/til.js
@@ -1,14 +1,16 @@
 'use strict';
-// 定数　const
-// 変数　let
 
-let price = 150;
-console.log(price * 140);
-console.log(price * 160);
+let price = 500;
+// price = price += 100;
+price += 100;
 
+price *= 2;
 
-price = 170;
-console.log(price * 140);
-console.log(price * 160);
+price += 1;
+price ++;
 
+price -= 1;
+price --;
 
+console.log(price);
+//1200
