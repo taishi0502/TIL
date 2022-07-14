c156239b0ff6fd61e142403b4dcffdb6d40efc67
taishi0502 <kimutai_71@icloud.com>
2022-07-14 19:32:13 +0900

    関数式

--- JS/JS文法.md
+++ JS/JS文法.md
@@ -261,6 +261,17 @@ const total = sum (1,2,3) + sum(3,4,5);
 console.log(total);
 ```
 
+# 関数式を使う
+```
+const sum = function(a,b,c){
+  return a + b + c;
+};
+
+const total = sum(1,2,3) + sum(3,4,5);
+console.log(total)
+```
+
+
 
 
 
--- JS/til.js
+++ JS/til.js
@@ -1,11 +1,8 @@
 'use strict';
 
-function sum(a, b, c){
+const sum = function(a,b,c){
   return a + b + c;
 };
 
-sum (1,2,3)
-sum (3,4,5)
-
-const total = sum (1,2,3) + sum(3,4,5);
-console.log(total);
+const total = sum(1,2,3) + sum(3,4,5);
+console.log(total)
