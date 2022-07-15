411f8ffe5c955ab50fbd0cfe44632358e4698321
taishi0502 <kimutai_71@icloud.com>
2022-07-15 12:05:31 +0900

    配列の要素を文字列として結合

--- JS/til.js
+++ JS/til.js
@@ -1,7 +1,9 @@
 'use strict';
 
-let x = [1, 2];
-let y = x;
-x[0] = 5;
-console.log(x) //[5, 2]
-console.log(y) //[5, 2]  <<<[1, 2]ではない
+const d = [2019, 11, 14]
+console.log(d.join('/'))
+// >>> 2019/11/4
+
+const t = '17:08:24'
+console.log(t.split(':'))
+// >>> ['17', '08', '24']
