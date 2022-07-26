06a7d709358b03dd8172feee34cbf7d189d4c640
taishi0502 <kimutai_71@icloud.com>
2022-07-26 14:25:29 +0900

    TIL_Docker

---
+++ Dockerについて/learning.md
@@ -0,0 +1,16 @@
+# Docker
+
+*概要
+- 軽量な仮想化環境
+- Build once, run anywhere
+
+※公式サイト
+
+*知識
+- Unix
+- Vim
+- Vagrant
+
+*環境
+- Ubuntu
+
--- JS/til.js
+++ JS/til.js
@@ -11,14 +11,47 @@ class Post {
     show() {
       console.log(`${this.text} - ${this.likeCount}いいね`)
     }
+
+    like(){
+      this.likeCount++;
+    }
+    //静的メソッド
+    //thisは使えない
+   static showInfo(){
+      console.log('Post class version 1.0')
+    }
+  }
+class SponsoredPost extends Post {
+  
+    constructor(text, sponsored){
+      super();
+    this.text = text;
+    this.likeCount = 0;
+    }
+
+    show() {
+      console.log(`${this.text} - ${this.likeCount}いいね`)
+      console.log(`...sponsored by ${this.sponsore}`)
+    }
+
+    like(){
+      this.likeCount++;
+    }
+    //静的メソッド
+    //thisは使えない
+   static showInfo(){
+      console.log('Post class version 1.0')
+    }
   }
 
  const posts = [
    new Post('JavaScriptの勉強中'),
-   new Post('プログラミング楽しい')
+   new Post('プログラミング楽しい'),
+   new SponsoredPost('３分動画でマスターしよう', 'test')
  ];
 
+// posts[0].like();
 
-posts[0].show();
-posts[1].show();
+posts[2].show();
+posts[2].like();
 
